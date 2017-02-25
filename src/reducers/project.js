d786d424e52e273580f1f078c19b4248f313c487
Cody Loyd <codyloyd@gmail.com>
2017-02-25 05:44:43 -0600

    add project reducer file and test

---
+++ src/reducers/project.js
@@ -0,0 +1,13 @@
+const defaultState = {}
+
+const project = (state = defaultState, action) => {
+  const { type } = action
+  switch (type) {
+    default:
+      return state
+  }
+}
+
+export default {
+  project
+}
