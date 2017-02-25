c79b38bb2bd75a96e6a42327535ae45a81605d60
Cody Loyd <codyloyd@gmail.com>
2017-02-25 07:07:53 -0600

    add allId and tests

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -13,3 +13,13 @@ export const byId = (state = defaultState.byId, action) => {
       return state
   }
 }
+
+export const allIds = (state = defaultState.allIds, action) => {
+  const { type, payload } = action
+  switch (type) {
+    case 'CREATE_PROJECT':
+      return [...state, payload.id]
+    default:
+      return state
+  }
+}
--- src/reducers/projects.tests.js
+++ src/reducers/projects.tests.js
@@ -1,8 +1,8 @@
 import test from 'tape'
 import deepFreeze from 'deep-freeze'
-import { byId } from './projects'
+import { byId, allIds } from './projects'
 
-test('byId should be able to create new project', assert => {
+test('byId', assert => {
   const msg = 'calling CREATE_PROJECT with byId should return an object with the new project indexed by ID'
   const stateBefore = {}
   deepFreeze(stateBefore)
@@ -41,7 +41,7 @@ test('byId should be able to create new project', assert => {
   assert.end()
 })
 
-test('byId should be able to create new project when others exist', assert => {
+test('byId', assert => {
   const msg = 'calling CREATE_PROJECT with byId should return an object with the new project indexed by ID'
   const stateBefore = {
     '1': {
@@ -101,6 +101,58 @@ test('byId should be able to create new project when others exist', assert => {
     }
   }
   const actual = byId(stateBefore, action)
+  assert.deepEqual(actual, expected, msg)
+  assert.end()
+})
+
+test('allIds', assert => {
+  const msg = 'CREATE_PROJECT should add IDs to the allIds array'
+  const stateBefore = []
+  deepFreeze(stateBefore)
+  const action = {
+    type: 'CREATE_PROJECT',
+    payload: {
+      id: '1',
+      ownerId: '1',
+      name: 'project name',
+      description: 'project description',
+      summary: 'project summary',
+      status: 'open',
+      roles: ['123', '456'],
+      tags: ['tag1', 'tag2'],
+      timeStamp: '123',
+      lastUpdated: '123'
+    }
+  }
+  deepFreeze(action)
+  const expected = ['1']
+  const actual = allIds(stateBefore, action)
+  assert.deepEqual(actual, expected, msg)
+  assert.end()
+})
+
+test('allIds', assert => {
+  const msg = 'CREATE_PROJECT should add IDs to the allIds array'
+  const stateBefore = ['1']
+  deepFreeze(stateBefore)
+  const action = {
+    type: 'CREATE_PROJECT',
+    payload: {
+      id: '2',
+      ownerId: '1',
+      name: 'project name',
+      description: 'project description',
+      summary: 'project summary',
+      status: 'open',
+      roles: ['123', '456'],
+      tags: ['tag1', 'tag2'],
+      timeStamp: '123',
+      lastUpdated: '123'
+    }
+  }
+  deepFreeze(action)
+  const expected = ['1', '2']
+  const actual = allIds(stateBefore, action)
   assert.deepEqual(actual, expected, msg)
   assert.end()
 })
