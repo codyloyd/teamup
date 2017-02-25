f1ebaa19156f29ae27882f10f64f29fda421813e
Cody Loyd <codyloyd@gmail.com>
2017-02-24 20:59:14 -0600

    add database prototype

---
+++ static/mockDB.js
@@ -0,0 +1,66 @@
+db = {
+  projects: {
+    projectId: {
+      id: '01',
+      ownerId: '1234',
+      name: 'project name',
+      description: 'project description.....',
+      summary: 'project summary.....',
+      roles: [
+        'roleId1',
+        'roleId2'
+      ],
+      tags: [
+        'tag1',
+        'tag2',
+        'tag3'
+      ],
+      timeStamp: 123456,
+      lastUpdated: 123456
+    }
+  },
+
+  roles: {
+    roleId1: {
+      id: 'roleId1',
+      projectId: 'projectId1',
+      name: 'role name',
+      status: 'role status',
+      description: 'role description',
+      applications: [
+        'applicationId1',
+        'applicationId2'
+      ],
+      'users': null // to be filled with the name of the user that gets accepted
+    }
+  },
+
+  applications: {
+    '1': {
+      userId: '1',
+      roleId: '1',
+      timeStamp: 123456
+    }
+  },
+
+  users: {
+    userId1: {
+      id: 'userId1',
+      displayName: 'Name',
+      githubUsername: 'gh-name',
+      signedUp: 12345,
+      projectsOwned: [
+        'projectId1',
+        'projectId2'
+      ],
+      applications: [
+        'applicationId1',
+        'applicationId2'
+      ],
+      roles: [
+        'roleId3',
+        'roleId4'
+      ]
+    }
+  }
+}
