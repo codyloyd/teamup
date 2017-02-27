b35005862aeae01d7a815914a09c422403a01499
Cody Loyd <codyloyd@gmail.com>
2017-02-27 08:02:04 -0600

    remove single project reducer, rework projects
    updated the projects reducer to deal with fetching from the API, as it no longer needs to deal directly with creating a new project.

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -1,61 +1,69 @@
 import {combineReducers} from 'redux'
-import cuid from 'cuid'
-import project from './project'
+import * as api from '../api'
 
 // ** action types **
-const CREATE_PROJECT = 'CREATE_PROJECT'
+export const FETCH_PROJECTS_REQUESTED = 'FETCH_PROJECTS_REQUESTED'
+export const FETCH_PROJECTS_SUCCESSFUL = 'FETCH_PROJECTS_SUCCESSFUL'
+export const FETCH_PROJECTS_FAILED = 'FETCH_PROJECTS_FAILED'
+
 // ** action creators **
-export const createProject = ({
-  ownerId = '',
-  name = '',
-  summary = '',
-  tags = [],
-  description = '',
-  roles = [],
-  id = cuid(),
-  status = 'open',
-  timeStamp = Date.now(),
-  lastUpdated = Date.now()
-}) => (
-  {
-    type: CREATE_PROJECT,
-    payload: {
-      id,
-      ownerId,
-      name,
-      summary,
-      tags,
-      description,
-      roles,
-      status,
-      timeStamp,
-      lastUpdated
-    }
-  }
-)
+
+// don't think we'll need this since it's done by the API lol
+// keeping it here momentarily for the sake of documentation.
+// export const createProject = ({
+//   ownerId = '',
+//   name = '',
+//   summary = '',
+//   tags = [],
+//   description = '',
+//   roles = [],
+//   id = cuid(),
+//   status = 'open',
+//   timeStamp = Date.now(),
+//   lastUpdated = Date.now()
+// }) => (
+//   {
+//     type: CREATE_PROJECT,
+//     payload: {
+//       id,
+//       ownerId,
+//       name,
+//       summary,
+//       tags,
+//       description,
+//       roles,
+//       status,
+//       timeStamp,
+//       lastUpdated
+//     }
+//   }
+// )
+
+// ** async actions **
+// these use thunks
+export const fetchProject = (id) => (dispatch) => {
+  api.fetchProject(id).then(response => {
+    dispatch({
+      type: 'FETCH_PROJECT_SUCCESSFUL',
+      response
+    })
+  })
+}
 
 const defaultState = {
   byId: {},
   allIds: []
 }
+
 export const byId = (state = defaultState.byId, action) => {
-  const { type, payload } = action
+  const { type, response } = action
   switch (type) {
-    case 'CREATE_PROJECT':
-      return {...state, [payload.id]: project(state[payload.id], action)}
+    case FETCH_PROJECTS_SUCCESSFUL:
+      return {...state, ...response}
     default:
       return state
   }
 }
 
-export const allIds = (state = defaultState.allIds, action) => {
-  const { type, payload } = action
-  switch (type) {
-    case 'CREATE_PROJECT':
-      return [...state, payload.id]
-    default:
-      return state
-  }
-}
 
-export const projects = combineReducers({byId, allIds})
+export const projects = combineReducers({byId})
--- src/reducers/projects.tests.js
+++ src/reducers/projects.tests.js
@@ -3,204 +3,97 @@ import deepFreeze from 'deep-freeze'
 import { byId, allIds, projects } from './projects'
 
 describe('byId', ({test}) => {
-  test('with empty state', assert => {
-    const msg = 'calling CREATE_PROJECT with byId should return an object with the new project indexed by ID'
+  test('FETCH_PROJECTS_SUCCESSFUL', (assert) => {
+    const msg = 'successful fetching of projects should load projects into db'
     const stateBefore = {}
     deepFreeze(stateBefore)
     const action = {
-      type: 'CREATE_PROJECT',
-      payload: {
-        id: '1',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      }
-    }
-    deepFreeze(action)
-    const expected = {
-      '1': {
-        id: '1',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      }
-    }
-    const actual = byId(stateBefore, action)
-    assert.deepEqual(actual, expected, msg)
-    assert.end()
-  })
-
-  test('with items in state', assert => {
-    const msg = 'calling CREATE_PROJECT with byId should return an object with the new project indexed by ID'
-    const stateBefore = {
-      '1': {
-        id: '1',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      }
-    }
-    deepFreeze(stateBefore)
-    const action = {
-      type: 'CREATE_PROJECT',
-      payload: {
-        id: '2',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      }
-    }
-    deepFreeze(action)
-    const expected = {
-      '1': {
-        id: '1',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      },
-      '2': {
-        id: '2',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      }
-    }
-    const actual = byId(stateBefore, action)
-    assert.deepEqual(actual, expected, msg)
-    assert.end()
-  })
-})
-
-describe('allIds', ({test}) => {
-  test('with empty state', assert => {
-    const msg = 'CREATE_PROJECT should add IDs to the allIds array'
-    const stateBefore = []
-    deepFreeze(stateBefore)
-    const action = {
-      type: 'CREATE_PROJECT',
-      payload: {
-        id: '1',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      }
-    }
-    deepFreeze(action)
-    const expected = ['1']
-    const actual = allIds(stateBefore, action)
-    assert.deepEqual(actual, expected, msg)
-    assert.end()
-  })
-
-  test('with full state', assert => {
-    const msg = 'CREATE_PROJECT should add IDs to the allIds array'
-    const stateBefore = ['1']
-    deepFreeze(stateBefore)
-    const action = {
-      type: 'CREATE_PROJECT',
-      payload: {
-        id: '2',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      }
-    }
-    deepFreeze(action)
-    const expected = ['1', '2']
-    const actual = allIds(stateBefore, action)
-    assert.deepEqual(actual, expected, msg)
-    assert.end()
-  })
-})
-
-describe('projects()', ({test}) => {
-  test('reducers are combined properly', assert => {
-    const msg = 'calling CREATE PROJECT on the combined reducers works as expected'
-    const stateBefore = {}
-    deepFreeze(stateBefore)
-    const action = {
-      type: 'CREATE_PROJECT',
-      payload: {
-        id: '1',
-        ownerId: '1',
-        name: 'project name',
-        description: 'project description',
-        summary: 'project summary',
-        status: 'open',
-        roles: ['123', '456'],
-        tags: ['tag1', 'tag2'],
-        timeStamp: '123',
-        lastUpdated: '123'
-      }
-    }
-    deepFreeze(action)
-    const expected = {
-      byId: {
-        '1': {
-          id: '1',
-          ownerId: '1',
+      type: 'FETCH_PROJECTS_SUCCESSFUL',
+      response: {
+        '01': {
+          id: '01',
+          ownerId: '1234',
           name: 'project name',
-          description: 'project description',
-          summary: 'project summary',
-          status: 'open',
-          roles: ['123', '456'],
-          tags: ['tag1', 'tag2'],
-          timeStamp: '123',
-          lastUpdated: '123'
+          description: 'project description.....',
+          summary: 'project summary.....',
+          status: 'open', // open/closed
+          roles: [
+            'roleId1',
+            'roleId2'
+          ],
+          tags: [
+            'tag1',
+            'tag2',
+            'tag3'
+          ],
+          timeStamp: 123456,
+          lastUpdated: 123456
+        },
+        '02': {
+          id: '02',
+          ownerId: '1234',
+          name: 'project name',
+          description: 'project description.....',
+          summary: 'project summary.....',
+          status: 'open', // open/closed
+          roles: [
+            'roleId1',
+            'roleId2'
+          ],
+          tags: [
+            'tag1',
+            'tag2',
+            'tag3'
+          ],
+          timeStamp: 123456,
+          lastUpdated: 123456
         }
-      },
-      allIds: ['1']
+      }
     }
-    const actual = projects(stateBefore, action)
+    deepFreeze(action)
+    const expected = {
+      '01': {
+        id: '01',
+        ownerId: '1234',
+        name: 'project name',
+        description: 'project description.....',
+        summary: 'project summary.....',
+        status: 'open', // open/closed
+        roles: [
+          'roleId1',
+          'roleId2'
+        ],
+        tags: [
+          'tag1',
+          'tag2',
+          'tag3'
+        ],
+        timeStamp: 123456,
+        lastUpdated: 123456
+      },
+      '02': {
+        id: '02',
+        ownerId: '1234',
+        name: 'project name',
+        description: 'project description.....',
+        summary: 'project summary.....',
+        status: 'open', // open/closed
+        roles: [
+          'roleId1',
+          'roleId2'
+        ],
+        tags: [
+          'tag1',
+          'tag2',
+          'tag3'
+        ],
+        timeStamp: 123456,
+        lastUpdated: 123456
+      }
+    }
+    const actual = byId(stateBefore, action)
     assert.deepEqual(actual, expected, msg)
     assert.end()
   })
 })
+
