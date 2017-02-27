1f168b3d8c09f5016bbe3c0c43f41fab210a704c
Cody Loyd <codyloyd@gmail.com>
2017-02-27 07:42:50 -0600

    fixed linting error
    we need to import the firebaseApp for it to work.. but we don't actually use the import so eslint was complaining about it.  I just added a comment to remove the eslint error.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './index.css'
-import firebaseApp from './firebase';
-import {fetchProjects} from './api'
-
-fetchProjects().then(data => console.log(data))
+// eslint-disable-next-line
+import firebaseApp from './firebase'
 
 ReactDOM.render(
   <App />,
