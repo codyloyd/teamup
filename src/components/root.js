76c1a171e9f38d8fdbbc782f7d1ee8cf4aa8a0cb
Kenny Lavender <leapingfish@gmail.com>
2017-03-04 12:21:28 -0800

    add profile

--- src/components/root.js
+++ src/components/root.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux'
 
 const Projects = props => (<h1>Projects</h1>)
 const Project = props => (<h1>Project</h1>)
+const Profile = props => (<h1>Profile</h1>)
 const NoMatch = props => (<h1>No Match</h1>)
 const LandingPage = props => (<h1>LandingPage</h1>)
 
@@ -22,6 +23,10 @@ export default React => ({store}) => {
           <Route path="project" component={Project}>
               <Route path="/view/:id" component={Project}/>
               <Route path="/edit/:id" component={Project}/>
+          </Route>
+          <Route path="profile" component={Profile}>
+              <Route path="/view/:id" component={Profile}/>
+              <Route path="/edit/:id" component={Profile}/>
           </Route>
           <Route path="*" component={NoMatch}/>
         </Route>
