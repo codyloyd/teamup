3a58c906aa05a71e65048329d76beb2558c4a30d
Cody Loyd <codyloyd@gmail.com>
2017-03-09 08:18:53 -0600

    linting

--- src/containers/ViewProject.js
+++ src/containers/ViewProject.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 import ProjectDetails from '../components/projectDetails'
 import RolesList from '../components/rolesList'
 import Loading from '../components/loading'
-import {fetchProjects} from '../reducers/projects'
-import {fetchRoles} from '../reducers/roles'
-
+import { fetchProjects } from '../reducers/projects'
+import { fetchRoles } from '../reducers/roles'
 
 class ViewProject extends React.Component {
   componentDidMount () {
-    const {fetchProjects, fetchRoles} = this.props
+    const { fetchProjects, fetchRoles } = this.props
     fetchProjects()
     fetchRoles(this.props.params.id)
   }
@@ -18,9 +17,9 @@ class ViewProject extends React.Component {
     if (this.props.isFetching) {
       return <Loading />
     }
-    const {title, summary, description, tags} = this.props.project
+    const { title, summary, description, tags } = this.props.project
     return (
-      <div className='view-project container'>
+      <div className="view-project container">
         <div className="column is-10 is-offset-1">
           <p className="heading">Project Details:</p>
           <ProjectDetails
@@ -30,31 +29,28 @@ class ViewProject extends React.Component {
             tags={tags || []}
           />
           <p className="heading">Open Roles:</p>
-          <RolesList roles={this.props.projectRoles || []}/>
+          <RolesList roles={this.props.projectRoles || []} />
         </div>
       </div>
     )
   }
 }
 
-
 // not sure where to put these
 const getSingleProject = (state, id) => {
-  const {entities: {projects: {byId}}} = state
-  return (byId && byId[id]) ? byId[id] : {}
+  const { entities: { projects: { byId } } } = state
+  return byId && byId[id] ? byId[id] : {}
 }
 const getProjectRoles = (state, id) => {
-  const {entities: {roles: {byId}}} = state
-  return (byId)
-    ? Object.values(byId).filter(r => r.projectId === id)
-    : []
+  const { entities: { roles: { byId } } } = state
+  return byId ? Object.values(byId).filter(r => r.projectId === id) : []
 }
-const getIsFetchingProjects = (state) => {
+const getIsFetchingProjects = state => {
   return state.entities.projects.isFetching
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const {params: {id}} = ownProps
+  const { params: { id } } = ownProps
   return {
     isFetching: getIsFetchingProjects(state),
     project: getSingleProject(state, id),
@@ -62,9 +58,6 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default
-  withRouter(
-    connect(
-      mapStateToProps,
-      {fetchProjects, fetchRoles}
-    )(ViewProject))
+export default withRouter(
+  connect(mapStateToProps, { fetchProjects, fetchRoles })(ViewProject)
+);
