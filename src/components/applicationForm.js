57f9d6693d011bd178cbaa078fcd71ec7568c66f
Cody Loyd <codyloyd@gmail.com>
2017-03-14 11:39:52 -0500

    update applicationForm

--- src/components/applicationForm.js
+++ src/components/applicationForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ApplicationForm = (
-  {createApplication, projectId, currentUser, roleId}
+  {createApplication, projectId, currentUser, roleId, roleName}
 ) => {
   let message
   return (
@@ -17,12 +17,16 @@ const ApplicationForm = (
         message.value = ''
       }}
     >
-      <label className="label">A short message to the project owner:</label>
+      <h1>Application for {roleName}</h1>
+      <p>
+        When you apply for this role, your profile information including your github profile will be sent to the project owner who will contact you if you are a good fit for the project.
+      </p>
       <div className="control">
         <textarea
           type="textarea"
           ref={input => message = input}
           className="textarea"
+          placeholder="Leave a short message to the project owner with your application."
         />
       </div>
       <div className="control">
