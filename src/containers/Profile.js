8ce9f4fbd5318e9d021d3f90fe2cfb902adee3ac
Cody Loyd <codyloyd@gmail.com>
2017-03-22 05:31:15 -0500

    fill in basic user info

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -8,7 +8,48 @@ class Profile extends React.Component {
   }
   render () {
     if (this.props.user) {
-      return <h1 className="title">{this.props.user.about}</h1>
+      return (
+        <div className="container">
+          <div className="column is-10 is-offset-1">
+            <div className="box">
+              <div className="columns">
+                <div className="column is-narrow">
+                  <figure className="image is-128x128">
+                    <img src={this.props.user.photoURL} alt="avatar" />
+                  </figure>
+                  <aside className="menu">
+                    <ul className="menu-list">
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-github fa-fw" />
+                          {' '}
+                          GitHub
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-link fa-fw" />
+                          {' '}Personal Website
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-linkedin fa-fw" />
+                          {' '}LinkedIn
+                        </a>
+                      </li>
+                    </ul>
+                  </aside>
+                </div>
+                <div className="column">
+                  <h1 className="title">{this.props.user.displayName}</h1>
+                  <p>{this.props.user.about}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
     }
     return null
   }
