0044eeaf83f4346abde808434b996aa240df94ed
Cody Loyd <codyloyd@gmail.com>
2017-03-22 19:28:05 -0500

    add links to profile page

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -17,32 +17,13 @@ class Profile extends React.Component {
                   <figure className="image is-128x128">
                     <img src={this.props.user.photoURL} alt="avatar" />
                   </figure>
-                  <aside className="menu">
-                    <ul className="menu-list">
-                      <li>
-                        <a href="#">
-                          <i className="fa fa-github fa-fw" />
-                          {' '}
-                          GitHub
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i className="fa fa-link fa-fw" />
-                          {' '}Personal Website
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i className="fa fa-linkedin fa-fw" />
-                          {' '}LinkedIn
-                        </a>
-                      </li>
-                    </ul>
-                  </aside>
+                  <Links
+                    links={this.props.user.links}
+                    email={this.props.user.email}
+                  />
                 </div>
                 <div className="column">
-                  <h1 className="title">{this.props.user.displayName}</h1>
+                  <h1 className="title">{this.props.user.displayName || ''}</h1>
                   <p>{this.props.user.about}</p>
                 </div>
               </div>
@@ -53,6 +34,43 @@ class Profile extends React.Component {
     }
     return null
   }
+}
+
+const Links = ({links, email}) => {
+  return (
+    <aside className="menu">
+      <ul className="menu-list">
+        <li>
+          <a href={`mailto:${email}`}>
+            <i className="fa fa-envelope-o fa-fw" />
+            {' '}{email}
+          </a>
+        </li>
+        {links
+          ? links.map(link => <ProfileLink key={link.url} link={link} />)
+          : null}
+      </ul>
+    </aside>
+  )
+}
+
+const icons = {
+  GitHub: 'fa-github',
+  LinkedIn: 'fa-linkedin',
+  Twitter: 'fa-twitter',
+  Facebook: 'fa-facebook-official',
+  'Stack Overflow': 'fa-stack-overflow'
+}
+
+const ProfileLink = ({link}) => {
+  return (
+    <li>
+      <a href={link.url}>
+        <i className={`fa fa-fw ${icons[link.name] || 'fa-link'}`} />
+        {' '}{link.name}
+      </a>
+    </li>
+  )
 }
 
 const mapStateToProps = (state, ownProps) => {
